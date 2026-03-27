253b53c0ef82dc6761c13ffa4a2bb4554ab5f35e
Dinesh Patil <Dinesh Patil@Gyrussurat.net>
2026-03-28 03:44:11 +0530

    IDP Training Count Update

--- src/main/java/com/qa/gyruslearner/pages/IDPPage.java
+++ src/main/java/com/qa/gyruslearner/pages/IDPPage.java
@@ -1,11 +1,13 @@
 package com.qa.gyruslearner.pages;
 
+import java.time.Duration;
 import java.util.List;
 
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.qa.gyruslearner.base.TestBase;
 import com.qa.gyruslearner.constants.AppConstants;
@@ -28,27 +30,29 @@ public class IDPPage extends TestBase {
 
 	@FindBy(xpath = "//span[contains(@data-key,'menuIDP')]")
 	WebElement lnkIDP;
-	
+
 	@FindBy(xpath = "//p[normalize-space()='Back to Dashboard']")
 	WebElement lnkBackToDashboard;
-	
+
 	@FindBy(xpath = "//*[normalize-space()='Individual Development Plan']")
 	WebElement idpHeaderPageTitle;
-	
+
 	@FindBy(xpath = "//div[contains(@class,'IDE-header')]//p")
 	WebElement trainingcount;
-	
+
 	@FindBy(xpath = "//gyrusaim-cardview//div[@title='Card View']")
 	List<WebElement> idpCardViewList;
-	
-	@FindBy(xpath = "//gyrusaim-localfilter//input[@type='checkbox']")
-	List<WebElement> QuickFilterlist;
-	
+
+	@FindBy(id = "lbl_156")
+	WebElement btnQFAllELearning;
+
+	@FindBy(xpath = "//button[normalize-space()='See More']")
+	List<WebElement> btnSeeMore;
 
 	public boolean isMyLearningMenuDisplay() {
 
 		jsUtil.scrollIntoViewCenter(lnkMyLearning);
-		return eleUtil.visibleElementWhenReady(lnkMyLearning,AppConstants.MEDIUM_TIME_OUT);
+		return eleUtil.visibleElementWhenReady(lnkMyLearning, AppConstants.MEDIUM_TIME_OUT);
 	}
 
 	public boolean isMyLearningMenuEnable() {
@@ -60,7 +64,7 @@ public class IDPPage extends TestBase {
 	public void doclickMyLearningSubmenu() {
 
 		jsUtil.scrollIntoViewCenter(lnkMyLearning);
-		eleUtil.clickElementWhenReady(lnkMyLearning,AppConstants.MEDIUM_TIME_OUT);
+		eleUtil.clickElementWhenReady(lnkMyLearning, AppConstants.MEDIUM_TIME_OUT);
 	}
 
 	public String getIDPPageUrl() {
@@ -82,7 +86,7 @@ public class IDPPage extends TestBase {
 	public boolean isIDPMenuDisplay() {
 
 		jsUtil.scrollIntoViewCenter(lnkIDP);
-		return eleUtil.visibleElementWhenReady(lnkIDP,AppConstants.MEDIUM_TIME_OUT);
+		return eleUtil.visibleElementWhenReady(lnkIDP, AppConstants.MEDIUM_TIME_OUT);
 	}
 
 	public boolean isIDPMenuEnable() {
@@ -94,46 +98,142 @@ public class IDPPage extends TestBase {
 	public void doclickIDPSubMenu() {
 
 		jsUtil.scrollIntoViewCenter(lnkIDP);
-		eleUtil.clickElementWhenReady(lnkIDP,AppConstants.MAX_TIME_OUT);
+		eleUtil.clickElementWhenReady(lnkIDP, AppConstants.MAX_TIME_OUT);
 	}
-	
+
 	public boolean isBackToDashboardDisplay() {
 
 		jsUtil.scrollIntoViewCenter(lnkBackToDashboard);
 		return eleUtil.visibleElementWhenReady(lnkBackToDashboard, AppConstants.MEDIUM_TIME_OUT);
 	}
+
 	public void doclickBackToDashboard() {
 
 		eleUtil.waitForLoaderToDisappear();
 		jsUtil.scrollIntoViewCenter(lnkBackToDashboard);
-		eleUtil.clickElementWhenReady(lnkBackToDashboard,AppConstants.MEDIUM_TIME_OUT);
+		eleUtil.clickElementWhenReady(lnkBackToDashboard, AppConstants.MEDIUM_TIME_OUT);
 	}
-	
+
 	public boolean isIDPHeaderPageTitleDisplay() {
 
 		jsUtil.scrollIntoViewCenter(idpHeaderPageTitle);
 		return eleUtil.visibleElementWhenReady(idpHeaderPageTitle, AppConstants.MEDIUM_TIME_OUT);
 	}
-	
+
 	public String getIDPHeaderPageTitle() {
-		
-		return eleUtil.doGetElementText(idpHeaderPageTitle); 
+
+		return eleUtil.doGetElementText(idpHeaderPageTitle);
 	}
-	
+
 	public boolean isTrainingCountDisplay() {
 
 		jsUtil.scrollIntoViewCenter(trainingcount);
 		return eleUtil.visibleElementWhenReady(trainingcount, AppConstants.MEDIUM_TIME_OUT);
 	}
-	public String getIDPTrainingCount() {
-		
+
+	public int getTotalTrainingCount() {
+
 		eleUtil.waitForLoaderToDisappear();
-		eleUtil.waitForElementVisible(trainingcount, AppConstants.MEDIUM_TIME_OUT);	
-		return eleUtil.doGetElementText(trainingcount); 
+		eleUtil.visibleElementWhenReady(trainingcount, AppConstants.MAX_TIME_OUT);
+		
+		String text = eleUtil.doGetElementText(trainingcount);
+		
+		return Integer.parseInt(text.replaceAll("\\D+", ""));
+		
+		/*
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	       
+		// Wait until text is updated (not empty / not old)
+	    wait.until(driver -> {
+	        String text = driver.findElement(By.xpath("//div[contains(@class,'IDE-header')]//p")).getText();
+	        return text != null && !text.trim().isEmpty();
+	    });
+
+	    String text = driver.findElement(By.xpath("//div[contains(@class,'IDE-header')]//p")).getText();
+		*/
 	}
-	
-	
-	
-	
+
+	public boolean isAllELearningDisplay() {
+
+		eleUtil.waitForLoaderToDisappear();
+		jsUtil.scrollIntoViewCenter(btnQFAllELearning);
+		return eleUtil.visibleElementWhenReady(btnQFAllELearning, AppConstants.MEDIUM_TIME_OUT);
+	}
+
+	public boolean isAllELearningEnable() {
+
+		eleUtil.waitForLoaderToDisappear();
+		jsUtil.scrollIntoViewCenter(btnQFAllELearning);
+		return eleUtil.isElementEnable(btnQFAllELearning);
+	}
+
+	public void doclickAllELearningQuickFilter() {
+
+		eleUtil.waitForLoaderToDisappear();
+		jsUtil.scrollIntoViewCenter(btnQFAllELearning);
+		eleUtil.clickElementWhenReady(btnQFAllELearning, AppConstants.MEDIUM_TIME_OUT);
+		eleUtil.waitForLoaderToDisappear();
+	}
+
+	public int getCardCount() {
+
+		eleUtil.waitForLoaderToDisappear();
+		// Wait until new cards loaded
+		new WebDriverWait(driver, Duration.ofSeconds(AppConstants.MAX_TIME_OUT))
+				.until(driver -> idpCardViewList.size());
+		return idpCardViewList.size();
+	}
+
+	public void waitForCardsToLoad(int previousCount) {
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(AppConstants.MAX_TIME_OUT));
+
+		wait.until(driver -> {
+			int current = idpCardViewList.size();
+			return current > previousCount;
+		});
+	}
+
+	public void loadAllCards() {
+
+		eleUtil.waitForLoaderToDisappear();
+
+		int previousCount = 0;
+
+		while (true) {
+
+			int currentCount = idpCardViewList.size();
+
+			if (currentCount == previousCount) {
+				break;
+			}
+
+			previousCount = currentCount;
+
+			if (btnSeeMore.isEmpty())
+				break;
+
+			WebElement btn = btnSeeMore.get(0);
+			try {
+				jsUtil.scrollIntoViewCenter(btn);
+				// ((JavascriptExecutor)
+				// driver).executeScript("arguments[0].scrollIntoView({block:'center'});", btn);
+				eleUtil.waitForLoaderToDisappear();
+				btn.click();
+				eleUtil.waitForLoaderToDisappear();
+
+				waitForCardsToLoad(previousCount);
+				// Wait until new cards loaded
+				new WebDriverWait(driver, Duration.ofSeconds(AppConstants.MAX_TIME_OUT))
+						.until(driver -> idpCardViewList.size() > currentCount);
+				eleUtil.waitForLoaderToDisappear();
+
+			} catch (Exception e) {
+				System.out.println("See More click failed, retrying...");
+				break;
+			}
+
+		}
+	}
 
 }
--- src/test/java/com/qa/gyruslearner/testcases/IDPPageTestCase.java
+++ src/test/java/com/qa/gyruslearner/testcases/IDPPageTestCase.java
@@ -1,11 +1,11 @@
 package com.qa.gyruslearner.testcases;
 
-
 import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import com.qa.gyruslearner.base.TestBase;
@@ -13,52 +13,54 @@ import com.qa.gyruslearner.constants.AppConstants;
 import com.qa.gyruslearner.pages.DashboardPage;
 import com.qa.gyruslearner.pages.IDPPage;
 import com.qa.gyruslearner.pages.LoginPage;
+import com.qa.gyruslearner.util.ElementUtil;
 
 public class IDPPageTestCase extends TestBase {
 
 	LoginPage loginpage;
 	DashboardPage dashboard;
 	IDPPage idp;
-	
+	ElementUtil eleUtil;
+
 	public IDPPageTestCase() {
 		super();
 	}
-	
+
 	@BeforeClass
 	public void setUp() {
 		initialazation();
 		loginpage = new LoginPage();
-		dashboard = new DashboardPage();	
+		dashboard = new DashboardPage();
 		idp = new IDPPage();
+		eleUtil = new ElementUtil();
 	}
+
 	@BeforeMethod
 	public void pageRefresh() {
 
-		
 		if (driver.getCurrentUrl().equals(AppConstants.LOGIN_PAGE_URL)) {
 			loginpage.getUserFirstTimeLogin("TTeam01", "123");
-				
+
 		} else if (driver.getCurrentUrl().equals(AppConstants.DASHBOARD_PAGE_URL)) {
-			
-				System.out.println("Current Page is display");
+
+			System.out.println("Current Page is display");
 		}
 	}
-	
-	
+
 	@Test(priority = 1)
 	public void verifyMyLearnerLinkTest() {
-		
+
 		Assert.assertTrue(idp.isMyLearningMenuDisplay(), "MyLearning Menu icon  not visible");
 		Assert.assertTrue(idp.isMyLearningMenuEnable(), "MyLearning Submenu was not enabled");
 		idp.doclickMyLearningSubmenu();
 	}
-	
+
 	@Test(priority = 2)
 	public void verifyidpUrlTest() {
-		
+
 		Assert.assertTrue(idp.isIDPMenuDisplay(), "IDP SubMenu not visible");
 		Assert.assertTrue(idp.isIDPMenuEnable(), " IDP Submenu was not enabled");
-		idp.doclickIDPSubMenu();	
+		idp.doclickIDPSubMenu();
 		try {
 			String idpUrl = idp.getIDPPageUrl();
 			Assert.assertEquals(idpUrl, AppConstants.IDP_PAGE_URL);
@@ -67,17 +69,18 @@ public class IDPPageTestCase extends TestBase {
 			throw new SkipException("Skipping test because user could not login.");
 		}
 	}
-	
+
 	@Test(priority = 3)
 	public void verifyIDPPageTitileTest() {
-		
+
 		String idpPagetitle = idp.getIDPPageTitle();
 		Assert.assertEquals(idpPagetitle, AppConstants.IDP_PAGE_TITLE);
 	}
-	
+
+	@Ignore
 	@Test(priority = 4)
 	public void verifyBackToDashBoardTest() {
-		
+
 		Assert.assertTrue(idp.isBackToDashboardDisplay(), "Back to Dashboard button not visible");
 		idp.doclickBackToDashboard();
 		try {
@@ -92,26 +95,59 @@ public class IDPPageTestCase extends TestBase {
 		Assert.assertTrue(idp.isIDPMenuEnable(), " IDP Submenu was not enabled");
 		idp.doclickIDPSubMenu();
 	}
-	
+
 	@Test(priority = 5)
-	public void verifyIDPHeaderPageTitleAndTrainigCountTest() {
-		
+	public void verifyIDPHeaderPageTitleTest() {
+
 		Assert.assertTrue(idp.isIDPHeaderPageTitleDisplay(), "IDP Header Title  not visible");
-		String IDPHeaderPageTitle=idp.getIDPHeaderPageTitle();
-		Assert.assertEquals(IDPHeaderPageTitle,"Individual Development Plan");
+		String IDPHeaderPageTitle = idp.getIDPHeaderPageTitle();
+		Assert.assertEquals(IDPHeaderPageTitle, "Individual Development Plan");
 		System.out.println("IDP Page Header Title : " + IDPHeaderPageTitle);
-		Assert.assertTrue(idp.isTrainingCountDisplay(), "Trainings Count  not visible");
-		System.out.println("Trainings Count : " + idp.getIDPTrainingCount());
-		
+
 	}
 	@Test(priority = 6)
-	public void verifyQuickFilterTest() {
-		
-		
+	public void verifyAllCardLoadedAndCountTest() {
+
+		Assert.assertTrue(idp.isTrainingCountDisplay(), "Trainings Count  not visible");
+		System.out.println("------------------Display Number IDP Training Count -------------------");
+		int expectedCount = idp.getTotalTrainingCount();
+		System.out.println("Trainings Count : " + idp.getTotalTrainingCount());
+
+		idp.loadAllCards();
+		int actualCount = idp.getCardCount();
+
+		System.out.println("Expected: " + expectedCount);
+		System.out.println("Actual: " + actualCount);
+
+		Assert.assertEquals(actualCount, expectedCount, "Mismatch in training cards!");
+
 	}
 	
-	
-	
+	@Test(priority = 7)
+	public void verifyAllElearningQuickFilterTest() {
+
+		Assert.assertTrue(idp.isAllELearningDisplay(), "All ELearning  Quick Filter was not visible");
+		Assert.assertTrue(idp.isAllELearningEnable(), " All ELearning  Quick Filter  was not enabled");
+		idp.doclickAllELearningQuickFilter();
+		//Training count not load thread sleep put
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		int expectedCount = idp.getTotalTrainingCount();
+		System.out.println("Trainings Count : " + idp.getTotalTrainingCount());
+
+		idp.loadAllCards();
+		int actualCount = idp.getCardCount();
+
+		System.out.println("Expected: " + expectedCount);
+		System.out.println("Actual: " + actualCount);
+
+		Assert.assertEquals(actualCount, expectedCount, "Mismatch in training cards!");
+		
+	}
+
 	@AfterClass
 	public void tearDown() {
 		if (driver != null) {
@@ -119,6 +155,5 @@ public class IDPPageTestCase extends TestBase {
 		}
 
 	}
-	
-	
+
 }
